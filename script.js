52f016ed28efe277cf60e592bef2790c6834ec81
Shera Wisdom <Sheraokeaka16@gmail.com>
2022-02-04 04:47:08 -0800

    base commit

---
+++ script.js
@@ -0,0 +1,5 @@
+let name="Shera Wisdom";
+let date0fbirth="9th of May"
+let adress="Rumuagholu pipeline Port Harcout"
+
+document.write(`My name is  ${name}. I was born on ${date0fbirth}. I live in ${adress}.`)
